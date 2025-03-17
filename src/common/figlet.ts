428268d1da49858e8cadd2b6fd0e3c04b3d2dc15
Luis Henrique Ferreira de Freitas <guikipt@gmail.com>
2025-03-17 21:27:33 +0000

    [FIX] Fixing "DeprecationWarning: Calling promisify on a function that returns a Promise is likely a mistake."

--- src/common/figlet.ts
+++ src/common/figlet.ts
@@ -1,13 +1,13 @@
 import figlet from 'figlet';
-import { promisify } from 'util';
 import chalk from 'chalk';
-
-const figletAsync = promisify(figlet);
 
 export const getFigletText = async (text: string): Promise<string> => {
     try {
-        const result = await figletAsync(text);
-        return result || '';
+        return new Promise((resolve) => {
+            figlet.text(text, (err, result) => {
+                resolve(result || '');
+            });
+        });
     } catch (error) {
         console.error(
             chalk.red('Error generating ASCII art:'),
